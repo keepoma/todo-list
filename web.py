990b58f44b9ec0ab852db28221ae59a927a65aaf
NikKar <nikita.karuzin@gmail.com>
2023-09-27 16:07:05 -0600

    Create simple todo web app, implement todo list

--- web.py
+++ web.py
@@ -7,12 +7,19 @@ def add_todo():
     todos.append(sl.session_state["new_todo"] + '\n')
     fu.write_file(todos)
 
-
 sl.title("My To-Do App")
 sl.subheader("A little experiment")
 
-for todo in todos:
-    sl.checkbox(todo)
+for index, todo in enumerate(todos):
+    todo_in_list = str(index) + todo
+    print(todo_in_list)
+    checkbox = sl.checkbox(label=todo, key=todo_in_list)
+    if checkbox:
+        todos.pop(index)
+        fu.write_file(todos)
+        del sl.session_state[todo_in_list]
+        sl.experimental_rerun()
+
 
 sl.text_input(label="", placeholder="Write new todo here...",
               key="new_todo", on_change=add_todo)
