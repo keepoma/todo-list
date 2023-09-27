3e760c7b18a5c46116735a0a07252c56b59dba82
NikKar <nikita.karuzin@gmail.com>
2023-09-27 09:32:57 -0600

    Implement functional complete and exit buttons

--- gui.py
+++ gui.py
@@ -5,12 +5,15 @@ label = sg.Text("To-do")
 input_box = sg.Input(tooltip="Type here", key="todo")
 add_button = sg.Button("Add")
 list_box = sg.Listbox(values=fu.read_file(), key='todos',
-                      enable_events=True, size=(40, 12))
+                      enable_events=True, size=(45, 10))
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit", button_color="red")
 
 window = sg.Window("My To-Do App",
                    layout=[[label, input_box, add_button],
-                           [list_box, edit_button]],
+                           [list_box, edit_button],
+                           [complete_button, exit_button]],
                    font=("Helvetica", 16))
 
 while True:
@@ -34,7 +37,17 @@ while True:
             window['todos'].update(values=todos)
         case 'todos':
             window['todo'].update(value=values['todos'][0])
+        case 'Complete':
+            to_pop = values["todos"][0]
+            todos = fu.read_file()
+            todos.remove(to_pop)
+            #todos.pop(todos.index(to_pop))
+            fu.write_file(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value='')
         case sg.WIN_CLOSED:
+            break
+        case "Exit":
             break
 
 window.close()
