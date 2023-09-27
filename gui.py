6549b45abd718ee7c94cdffb11b7d3f7d3d8e6de
NikKar <nikita.karuzin@gmail.com>
2023-09-27 10:00:20 -0600

    Change theme, error handling, added clock

--- gui.py
+++ gui.py
@@ -1,6 +1,10 @@
 import functions as fu
 import PySimpleGUI as sg
+import time
 
+sg.theme('Black')
+
+clock = sg.Text('', key='clock')
 label = sg.Text("To-do")
 input_box = sg.Input(tooltip="Type here", key="todo")
 add_button = sg.Button("Add")
@@ -11,13 +15,15 @@ complete_button = sg.Button("Complete")
 exit_button = sg.Button("Exit", button_color="red")
 
 window = sg.Window("My To-Do App",
-                   layout=[[label, input_box, add_button],
+                   layout=[[clock],
+                           [label, input_box, add_button],
                            [list_box, edit_button],
                            [complete_button, exit_button]],
                    font=("Helvetica", 16))
 
 while True:
-    event, values = window.read()
+    event, values = window.read(timeout=200)
+    window['clock'].update(value=time.strftime("%Y, %b. %d  %H:%M:%S"))
     print("Event:", event)
     print("Values", values)
     print("---")
@@ -28,23 +34,29 @@ while True:
             fu.write_file(todos)
             window['todos'].update(values=todos)
         case 'Edit':
-            new_todo = values['todo']
-            todo_to_edit = values['todos'][0]
-            todos = fu.read_file()
-            index = todos.index(todo_to_edit)
-            todos[index] = new_todo
-            fu.write_file(todos)
-            window['todos'].update(values=todos)
+            try:
+                new_todo = values['todo']
+                todo_to_edit = values['todos'][0]
+                todos = fu.read_file()
+                index = todos.index(todo_to_edit)
+                todos[index] = new_todo
+                fu.write_file(todos)
+                window['todos'].update(values=todos)
+            except IndexError:
+                sg.popup("Select an item first", font=("Helvetica", 16))
         case 'todos':
             window['todo'].update(value=values['todos'][0])
         case 'Complete':
-            to_pop = values["todos"][0]
-            todos = fu.read_file()
-            todos.remove(to_pop)
-            #todos.pop(todos.index(to_pop))
-            fu.write_file(todos)
-            window['todos'].update(values=todos)
-            window['todo'].update(value='')
+            try:
+                to_pop = values["todos"][0]
+                todos = fu.read_file()
+                todos.remove(to_pop)
+                #todos.pop(todos.index(to_pop))
+                fu.write_file(todos)
+                window['todos'].update(values=todos)
+                window['todo'].update(value='')
+            except IndexError:
+                sg.popup("Select an item first", font=("Helvetica", 16))
         case sg.WIN_CLOSED:
             break
         case "Exit":
